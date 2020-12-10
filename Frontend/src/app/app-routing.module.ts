81e89780deb1da76104e30d389f09881c3821ca6
Evan Olijrhook <e.olijrhook@novi-education.nl>
2020-12-10 16:28:33 +0100

    klein beginnetje aan besmetting rapporteren

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {ReserveringComponent} from './component/reservering/reservering.componen
 import {PageNotFoundComponent} from './component/page-not-found/page-not-found.component';
 import {StartComponent} from './component/start/start.component';
 import {BezoekersComponent} from './component/bezoekers/bezoekers.component';
+import {BesmettingReportComponent} from './component/besmetting-report/besmetting-report.component';
 
 const routes: Routes = [
   {path: 'start', component: StartComponent},
   {path: 'reservering', component: ReserveringComponent},
   {path: 'bezoekers', component: BezoekersComponent},
+  {path: 'besmetting-report', component: BesmettingReportComponent},
   {path: 'not-found', component: PageNotFoundComponent},
   {path: '', redirectTo: '/start', pathMatch: 'full'},
   {path: '**', redirectTo: '/not-found'}
