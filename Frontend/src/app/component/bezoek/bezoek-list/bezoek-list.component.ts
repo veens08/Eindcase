f7c0151766b7d3a625cd0c385360798aef4d460e
jdh-dev <joshua.hully@gmail.com>
2020-12-16 17:21:05 +0100

    Frontend: reserveringen kunnen nu verwijderd worden, bezoekenlijst aangepast

--- Frontend/src/app/component/bezoek/bezoek-list/bezoek-list.component.ts
+++ Frontend/src/app/component/bezoek/bezoek-list/bezoek-list.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Bezoek} from '../../../model/Bezoek';
 import {BezoekService} from '../../../service/bezoek/bezoek.service';
+import {Persoon} from '../../../model/Persoon';
+import {ReserveringService} from '../../../service/reservering/reservering.service';
+import {Angular2MaterializeV1Service} from 'angular2-materialize-v1';
+import {IModal, ISelect} from 'angular2-materialize-v1/lib/IInstance';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-bezoek-list',
   templateUrl: './bezoek-list.component.html',
   styleUrls: ['./bezoek-list.component.scss']
 })
-export class BezoekListComponent implements OnInit {
+export class BezoekListComponent implements OnInit, AfterViewInit {
   searchCriterium = '';
   bezoeken: Bezoek[] = [];
+  gasten: Persoon[] = [];
+  modal: IModal;
+  potentieleBesmettingBezoek: Bezoek;
 
-  constructor(private bezoekService: BezoekService) { }
+  constructor(private bezoekService: BezoekService,
+              private angular2MaterializeService: Angular2MaterializeV1Service,
+              private router: Router){}
 
   ngOnInit(): void {
     this.bezoekService.getAll().subscribe(b => {
@@ -19,4 +29,17 @@ export class BezoekListComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit(): void {
+    this.modal = (this.angular2MaterializeService.initModal('#besmettingModal') as IModal);
+  }
+
+
+  besmettingDoorvoeren(p: Persoon): void {
+    this.router.navigate(['/besmetting-report']);
+  }
+
+  initializeBezoek(b: Bezoek): void {
+    this.potentieleBesmettingBezoek = b;
+    this.gasten = b.gasten;
+  }
 }
